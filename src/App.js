f0c910489460a820c60f6520b26ee7336cc8e20b
Ramit <ramitsarkar902@gmail.com>
2022-06-08 05:00:17 +0530

    added herosection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Navbar } from "./components";
+import { HeroSection, Navbar } from "./components";
 import "./app.scss";
 function App() {
   return (
     <div className="app flex-col">
       <Navbar />
-      <p>hello</p>
+
+      <HeroSection />
     </div>
   );
 }
