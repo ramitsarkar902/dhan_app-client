7f2d79d4b3bd366f2a50104c7553fc4b68918b12
Ramit <ramitsarkar902@gmail.com>
2022-06-10 06:50:48 +0530

    added the bar below about

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { About, HeroSection, Navbar } from "./components";
 import "./app.scss";
+import { RiBankLine } from "react-icons/ri";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
 function App() {
   return (
     <div className="app flex-col">
@@ -7,6 +10,20 @@ function App() {
 
       <HeroSection />
       <About />
+      <div className="about__last-rectangle">
+        <div className="flex justify-between items-center">
+          <RiBankLine className="about__last-rectangle-icon" />
+          <p>50+ Banks</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <AiFillEyeInvisible className="about__last-rectangle-icon" />
+          <p>Contactless KYC </p>
+        </div>
+        <div className="flex justify-between items-center">
+          <BiUserCircle className="about__last-rectangle-icon" />
+          <p>5,00,000+ Happy Customers</p>
+        </div>
+      </div>
     </div>
   );
 }
