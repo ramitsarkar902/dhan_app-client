c40afc3de166115adc528379f269d49c61ce4042
Ramit <ramitsarkar902@gmail.com>
2022-06-11 13:19:52 +0530

    footer done. just logo remain

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -23,6 +23,29 @@ const Footer = () => {
           <button>Subscribe</button>
         </div>
       </div>
+      <div className="footer__middle">
+        <div className="logo__footer">Logo later</div>
+        <div>
+          <h2>DHAN APP</h2>
+
+          <p>About</p>
+          <p>Careers</p>
+          <p>Contact</p>
+        </div>
+        <div>
+          <h2>CALCULATOR</h2>
+          <p>EMI Calculator</p>
+          <p>Home Loan Calculator</p>
+          <p>Personal Loan EMI Calculator</p>
+        </div>
+      </div>
+      <div className="borderLine"></div>
+      <div className="footer__bottom">
+        <div>
+          Privacy Policy | Terms of Use | Disclaimer | Intellectual Policy |
+          Sitemap
+        </div>
+      </div>
     </div>
   );
 };
